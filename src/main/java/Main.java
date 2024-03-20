7049c022f897746822fb15f0f3787f532a3212e7
lukaszkuzmiak <162459802+lukaszkuzmiak@users.noreply.github.com>
2024-03-20 13:31:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Main {
+  public static void main(String[] args) {
+    String str = "Hello world!";
+     char[] chars = str.toCharArray();
+     for (char c : chars) {
+      System.out.println(c);
+    }
+  }
+  
+}
